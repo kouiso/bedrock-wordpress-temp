6cccd00946dbbddbcd44acc3cdc9366007dbca60
KosukeIsogai <kouiso@ritmo.co.jp>
2024-03-27 20:25:50 +0900

    reset commit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# 依存関係のインストール
+composer install --no-interaction --prefer-dist --optimize-autoloader --working-dir=/var/www/html
+
 # WordPressのインストール
-wp core download --path=/var/www/html/web/wp --locale=ja --allow-root
+if [ ! -f "/var/www/html/web/wp/wp-config.php" ]; then
+    wp core download --path=/var/www/html/web/wp --locale=ja --allow-root
 
-# wp-config.phpの作成
-wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db:3306 --path=/var/www/html/web/wp --allow-root
+    # wp-config.phpの作成
+    wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db:3306 --path=/var/www/html/web/wp --allow-root
 
-# WordPressデータベースのインストール
-wp core install --url="http://localhost:8000" --title="サイトのタイトル" --admin_user="admin" --admin_password="password" --admin_email="info@example.com" --path=/var/www/html/web/wp --allow-root
+    # WordPressデータベースのインストール
+    wp core install --url="http://localhost:8000" --title="サイトのタイトル" --admin_user="admin" --admin_password="password" --admin_email="info@example.com" --path=/var/www/html/web/wp --allow-root
+else
+    echo "wp-config.php already exists."
+fi
 
 # その他の初期設定コマンド...
 
--- web/app/themes/ills/lib/js/bg-color.js
+++
@@ -1,8 +0,0 @@
-
-$(".check").on("click", function(){
-    $('.check').prop('checked', false);  //  全部のチェックを外す
-    $(this).prop('checked', true);  //  押したやつだけチェックつける
-    $('.label').removeClass('active');  //  クラスを取り除いて色を抜く
-    $(this).next('.label').addClass('active');//  選択状態の色を変える
-});
-
