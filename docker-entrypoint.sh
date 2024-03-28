62f593f0e316394f2c9492984f59438ac20c3917
KosukeIsogai <kouiso@ritmo.co.jp>
2024-03-28 13:41:48 +0900

    コミット

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+# .env ファイルから環境変数を読み込む
+set -a
+source /var/www/html/.env
+set +a
+
 # 依存関係のインストール
 composer install --no-interaction --prefer-dist --optimize-autoloader --working-dir=/var/www/html
 
+
 # WordPressのインストール
-if [ ! -f "/var/www/html/web/wp/wp-config.php" ]; then
-    wp core download --path=/var/www/html/web/wp --locale=ja --allow-root
+wp core install \
+    --url="${WP_SITEURL}" \
+    --title="${WP_SITETITLE}" \
+    --admin_user="${WP_ADMIN_USERNAME}" \
+    --admin_password="${WP_ADMIN_PASSWORD}" \
+    --admin_email="${WP_ADMIN_EMAIL}" \
+    --path="/var/www/html/web/wp" \
+    --allow-root
 
-    # wp-config.phpの作成
-    wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db:3306 --path=/var/www/html/web/wp --allow-root
-
-    # WordPressデータベースのインストール
-    wp core install --url="http://localhost:8000" --title="サイトのタイトル" --admin_user="admin" --admin_password="password" --admin_email="info@example.com" --path=/var/www/html/web/wp --allow-root
-else
-    echo "wp-config.php already exists."
-fi
-
-# その他の初期設定コマンド...
+# 検索エンジンがサイトをインデックスしないように設定
+wp option update blog_public 0 --path="/var/www/html/web/wp" --allow-root
 
 # Apacheを前景で実行
 exec apache2-foreground
